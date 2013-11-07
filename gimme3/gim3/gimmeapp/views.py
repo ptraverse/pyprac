2c1691868ba674c536f6aa32b5152e85b4aded29
root <root@ubuntuServer.(none)>
2013-11-06 23:12:03 -0800

    Added somre more

--- gimme3/gim3/gimmeapp/views.py
+++ gimme3/gim3/gimmeapp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render_to_response
-from django.shortcuts import render
 from django.core.context_processors import csrf
 from gimmeapp.models import Post
 from gimmeapp.models import User
@@ -7,28 +6,40 @@ from gimmeapp.models import Comment
 from gimmeapp.models import Like
 from gimmeapp.models import Lottery
 from django.template import RequestContext
+from gimmeapp.models import *
+
+def landing_page(request):
+	return render_to_response('preforms/form/landing_page.html' )
+
+def e404_page(request, othertext):
+	return render_to_response('preforms/form/e404_page.html' )
 
 def preform(request):
 	return render_to_response('preforms/form/form.html' )
 
 def create_post(request):
-	c = {}
-	c.update(csrf(request))
-	if request.method == 'POST': # If the form has been submitted...
-		form = PostForm(request.POST) # A form bound to the POST data
+	#c = {}
+	#c.update(csrf(request))
+	p = Post.objects.create()
+
+		#if request.method == 'POST': # If the form has been submitted...
+		# form = PostForm(request.POST) # A form bound to the POST data
+	p.name = form.element_2
+	user_created = form.element_1
+	#else:
+	p.name = 'defaultNewName'
+	p.user_created = 'requestmethodwasntpost'
+#		return HttpResponseRedirect('/thanks/') # Redirect after POST
 	#if form.is_valid(): # All validation rules pass
             # Process the data in form.cleaned_data
             # ...
 	#		return HttpResponseRedirect('/thanks/') # Redirect after POST
-	else:
-	        form = PostForm() # An unbound form
-	p = Post.objects.create()
-	p.name = form.element_2
-	p.user_created = form.element_1
+	# else:
+	        # form = PostForm() # An unbound form
 	p.save()
-	p.update(csrf(request))
 	postlist = Post.objects.all()
-	return render_to_response('allposts.html', c )
+	#return render_to_response('preforms/form/form.html', c )	
+	return render_to_response('preforms/form/form.html')
 
 def view_post(request,post):
 	p = Post.objects.get(id=post.id)
@@ -39,6 +50,7 @@ def view_all_posts(request):
 	if (len(postlist)==0):
 		p = Post.objects.create(name="primo")
 		p.save()
+		postlist = [] 
 		postlist.append(p)	
 	return render_to_response('allposts.html', { "postlist":postlist } )
 
@@ -47,4 +59,6 @@ def edit_post(request,post):
         return render_to_response('edit_post.html', {  "post":p } )
 
 def hello_world(request,word):
+	if (word==''):
+		word = 'DefaultWorldWord'
 	return render_to_response('hellow.html', { "word":word } )
--- gimme3/gim3/urls.py
+++ gimme3/gim3/urls.py
@@ -5,18 +5,17 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^post/(.+)/','gim3.gimmeapp.views.view_post'),
-	url(r'^hello_world/(.+)/','gim3.gimmeapp.views.hello_world'),
-	url(r'^allposts/','gim3.gimmeapp.views.view_all_posts'),
-    # Examples:
-    # url(r'^$', 'gim3.views.home', name='home'),
-    # url(r'^gim3/', include('gim3.foo.urls')),
+        url(r'^hello_world/(.*)','gim3.gimmeapp.views.hello_world'),            
+	url(r'^create-post','gim3.gimmeapp.views.create_post'),
+	url(r'^allposts','gim3.gimmeapp.views.view_all_posts'),		
+	url(r'^/post/(.*)','gim3.gimmeapp.views.view_post'),
+	url(r'^/preforms/create-post','gim3.gimmeapp.views.create_post'),	
+	url(r'^/preform/','gim3.gimmeapp.views.preform'),
+	# url(r'^(.+)','gim3.gimmeapp.views.e404_page'),
+	# url(r'','gim3.gimmeapp.views.landing_page'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-	url(r'^create-post/','gim3.gimmeapp.views.create_post'),
-	url(r'^/preforms/create-post/','gim3.gimmeapp.views.create_post'),
-	url(r'^preform/','gim3.gimmeapp.views.preform'),
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+	# url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	# url(r'^/admin/doc/', include('django.contrib.admindocs.urls')),
 )
